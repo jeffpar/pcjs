6a0c00ee4d29e3f18080cb49a1902ef0abccb3ea
Joe Auman <brain.recall@gmail.com>
2021-11-02 19:33:49 -0400

    cleaned up scipts and build, added docker-compose

--- docker-init.sh
+++ docker-init.sh
@@ -1,18 +1,11 @@
 #!/bin/sh
 
-echo "Cloning repo..."
-
-git clone https://github.com/jeffpar/pcjs.git
-chown -R jekyll:jekyll pcjs
-
-cd pcjs
-
-touch Gemfile.lock
-chmod a+w Gemfile.lock
+echo "Preparing..."
+chown -R jekyll:jekyll .
 
 echo "Building..."
 bundle install
 
 echo "Starting server..."
-bundle exec jekyll serve --host=0.0.0.0 --port $PCJS_PORT
+bundle exec jekyll serve --host=0.0.0.0
 
