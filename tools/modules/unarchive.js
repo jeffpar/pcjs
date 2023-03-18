d7e0a4b047197b84d89202ef946098f2f6497db0
Jeff Parsons <Jeff@pcjs.org>
2023-03-18 11:16:46 -0700

    Final cleanup of the Blast code (probably will never be used, but it was a good exercise)

--- tools/modules/unarchive.js
+++ tools/modules/unarchive.js
@@ -41,109 +41,12 @@ export default class Unarchive {
     }
 }
 
-/*
+/**
  * Blast functions for decompressing DCL_IMPLODE streams.
  *
  * Sadly, DCL_IMPLODE streams differ from the IMPLODE streams found in PKZIP files,
- * so this was a bit of a waste for my purposes, but perhaps someone else will find it useful.
+ * so this was a bit of a waste for my purposes, but perhaps someone else will find it useful. -JP
  */
-
-/**
- * Huffman table constructor for Blast.  count[1..MAXBITS] contains the number of
- * symbols of each length, which for a canonical code are stepped through in order.
- * symbol[] contains the symbol values in canonical order, where the number of entries
- * is the sum of the counts in count[].
- *
- * @class BlastHuffman
- * @property {Int16Array} count
- * @property {Int16Array} symbol
- * @property {number} left (number of possible codes left of current length)
- */
-class BlastHuffman {
-    /**
-     * Given a list of repeated code lengths rep[0..n-1], where each byte is a
-     * count (high four bits + 1) and a code length (low four bits), generate the
-     * list of code lengths.  This compaction reduces the size of the object code.
-     * Then given the list of code lengths length[0..n-1] representing a canonical
-     * Huffman code for n symbols, construct the tables required to decode those
-     * codes.  Those tables are the number of codes of each length, and the symbols
-     * sorted by length, retaining their original order within each length.  The
-     * return value is zero for a complete code set, negative for an over-
-     * subscribed code set, and positive for an incomplete code set.  The tables
-     * can be used if the return value is zero or positive, but they cannot be used
-     * if the return value is negative.  If the return value is zero, it is not
-     * possible for decode() using that table to return an error--any stream of
-     * enough bits will resolve to a symbol.  If the return value is positive, then
-     * it is possible for decode() using that table to return an error for received
-     * codes past the end of the incomplete lengths.
-     *
-     * @this {BlastHuffman}
-     * @param {number} nCounts (eg, MAXBITS)
-     * @param {number} nSymbols
-     * @param {number} rep
-     */
-    constructor(nCounts, nSymbols, rep)
-    {
-        let /* int */ symbol;                   // current symbol when stepping through length[]
-        let /* int */ len;                      // current length when stepping through h->count[]
-        let offs = new Array(nCounts+1);        // offsets in symbol table for each length
-        let length = new Array(256);            // code lengths
-
-        this.count = new Int16Array(nCounts+1);
-        this.symbol = new Int16Array(nSymbols);
-        this.left = 0;
-
-        /* convert compact repeat counts into symbol bit length list */
-        symbol = 0;
-        for (let i = 0; i < rep.length; i++) {
-            len = rep[i];
-            this.left = (len >> 4) + 1;
-            len &= 15;
-            do {
-                length[symbol++] = len;
-            } while (--this.left);
-        }
-        let n = symbol;
-
-        /* count number of codes of each length */
-        for (len = 0; len <= nCounts; len++) {
-            this.count[len] = 0;
-        }
-        for (symbol = 0; symbol < n; symbol++) {
-            (this.count[length[symbol]])++;     // assumes lengths are within bounds
-        }
-        if (this.count[0] == n) {               // no codes!
-            return;                             // complete, but decode() will fail
-        }
-
-        /* check for an over-subscribed or incomplete set of lengths */
-        this.left = 1;                          // one possible code of zero length
-        for (len = 1; len <= nCounts; len++) {
-            this.left <<= 1;                    // one more bit, double codes left
-            this.left -= this.count[len];       // deduct count from possible codes
-            if (this.left < 0) {
-                return;                         // over-subscribed (left > 0 means incomplete)
-            }
-        }
-
-        /* generate offsets into symbol table for each length for sorting */
-        offs[1] = 0;
-        for (len = 1; len < nCounts; len++) {
-            offs[len + 1] = offs[len] + this.count[len];
-        }
-
-        /*
-         * put symbols in table sorted by length, by symbol order within each length
-         */
-        for (symbol = 0; symbol < n; symbol++) {
-            if (length[symbol] != 0) {
-                this.symbol[offs[length[symbol]]++] = symbol;
-            }
-        }
-
-        /* left is zero for complete set, positive for incomplete set */
-    }
-}
 
 /**
  * @class Blast
@@ -158,18 +61,30 @@ class BlastHuffman {
  * @property {Uint8Array} out   // output array and sliding window
  */
 class Blast {
-
     static MAXBITS  = 13;       // maximum code length
     static MAXWIN   = 4096;     // maximum window size
-    static litcode = new BlastHuffman(Blast.MAXBITS, 256,
-       [11, 124, 8, 7, 28, 7, 188, 13, 76, 4, 10, 8, 12, 10, 12, 10, 8, 23, 8,
-        9, 7, 6, 7, 8, 7, 6, 55, 8, 23, 24, 12, 11, 7, 9, 11, 12, 6, 7, 22, 5,
-        7, 24, 6, 11, 9, 6, 7, 22, 7, 11, 38, 7, 9, 8, 25, 11, 8, 11, 9, 12,
-        8, 12, 5, 38, 5, 38, 5, 11, 7, 5, 6, 21, 6, 10, 53, 8, 7, 24, 10, 27,
-        44, 253, 253, 253, 252, 252, 252, 13, 12, 45, 12, 45, 12, 61, 12, 45, 44, 173]
-    );
-    static lencode = new BlastHuffman(Blast.MAXBITS, 16, [2, 35, 36, 53, 38, 23]);
-    static distcode = new BlastHuffman(Blast.MAXBITS, 64, [2, 20, 53, 230, 247, 151, 248]);
+
+    /**
+     * Huffman code decoding tables.  count[1..MAXBITS] contains the number
+     * of symbols of each length, which for a canonical code are stepped through
+     * in order.  symbol[] contains the symbol values in canonical order, where the
+     * number of entries is the sum of the counts in count[].
+     *
+     * To improve overall load time, we defer initializing the BlastHuffman tables
+     * until the first Blast constructor call (which may never even occur).
+     *
+     * @typedef {Object} BlastHuffman
+     * @property {Int16Array} count
+     * @property {Int16Array} symbol
+     * @property {number} left
+     */
+    static litcode;
+    static lencode;
+    static distcode;
+
+    /*
+     * Static tables used by decomp()
+     */
     static /* const short */ base = [3, 2, 4, 5, 6, 7, 8, 9, 10, 12, 16, 24, 40, 72, 136, 264]; // base for length codes
     static /* const char */ extra = [0, 0, 0, 0, 0, 0, 0, 0, 1, 2, 3, 4, 5, 6, 7, 8];           // extra bits for length codes
 
@@ -188,6 +103,17 @@ class Blast {
         this.next = 0;
         this.first = true;
         this.out = new Uint8Array(Blast.MAXWIN);
+        if (!Blast.litcode) {
+            Blast.litcode = Blast.constructHuffman(Blast.MAXBITS, 256,
+                [11, 124, 8, 7, 28, 7, 188, 13, 76, 4, 10, 8, 12, 10, 12, 10, 8, 23, 8,
+                 9, 7, 6, 7, 8, 7, 6, 55, 8, 23, 24, 12, 11, 7, 9, 11, 12, 6, 7, 22, 5,
+                 7, 24, 6, 11, 9, 6, 7, 22, 7, 11, 38, 7, 9, 8, 25, 11, 8, 11, 9, 12,
+                 8, 12, 5, 38, 5, 38, 5, 11, 7, 5, 6, 21, 6, 10, 53, 8, 7, 24, 10, 27,
+                 44, 253, 253, 253, 252, 252, 252, 13, 12, 45, 12, 45, 12, 61, 12, 45, 44, 173]
+             );
+             Blast.lencode = Blast.constructHuffman(Blast.MAXBITS, 16, [2, 35, 36, 53, 38, 23]);
+             Blast.distcode = Blast.constructHuffman(Blast.MAXBITS, 64, [2, 20, 53, 230, 247, 151, 248]);
+        }
     }
 
     /**
@@ -345,7 +271,7 @@ class Blast {
     /**
      * decode(h)
      *
-     * Decode a code from the stream s using huffman table h.  Return the symbol
+     * Decode a code from the stream using Huffman table h.  Return the symbol
      * or a negative value if there is an error.  If all of the lengths are zero,
      * i.e. an empty code, or if the code is incomplete and an invalid code is
      * received, then -9 is returned after reading MAXBITS bits.
@@ -364,7 +290,7 @@ class Blast {
      * more "natural" ordering starting with all zeros and incrementing.
      *
      * @this {Blast}
-     * @param {Huffman} h
+     * @param {BlastHuffman} h
      * @returns {number}
      */
     decode(h)
@@ -428,5 +354,102 @@ class Blast {
         }
         this.outbuf = Buffer.concat([this.outbuf, a]);
         return true;
+    }
+
+    /**
+     * constructHuffman(nCounts, nSymbols, rep)
+     *
+     * Given a list of repeated code lengths rep[0..n-1], where each byte is a
+     * count (high four bits + 1) and a code length (low four bits), generate the
+     * list of code lengths.  This compaction reduces the size of the object code.
+     * Then given the list of code lengths length[0..n-1] representing a canonical
+     * Huffman code for n symbols, construct the tables required to decode those
+     * codes.  Those tables are the number of codes of each length, and the symbols
+     * sorted by length, retaining their original order within each length.
+     *
+     * The return value is zero for a complete code set, negative for an over-
+     * subscribed code set, and positive for an incomplete code set.  The tables
+     * can be used if the return value is zero or positive, but they cannot be used
+     * if the return value is negative.  If the return value is zero, it is not
+     * possible for decode() using that table to return an error--any stream of
+     * enough bits will resolve to a symbol.  If the return value is positive, then
+     * it is possible for decode() using that table to return an error for received
+     * codes past the end of the incomplete lengths.
+     *
+     * Porting observations: the original "construct" function was supplied a table
+     * by the caller, whereas we allocate the table ourselves, so that's what we
+     * must return.  The numeric return value ("left") described above sounds useful,
+     * but the original code never examines it, so we could just drop it on the floor.
+     * However, perhaps someone will find it useful in the future, so we include it
+     * in the BlastHuffman table. -JP
+     *
+     * @param {number} nCounts (eg, MAXBITS)
+     * @param {number} nSymbols
+     * @param {number} rep
+     * @returns {BlastHuffman}
+     */
+    static constructHuffman(nCounts, nSymbols, rep)
+    {
+        let /* int */ symbol;                   // current symbol when stepping through length[]
+        let /* int */ len;                      // current length when stepping through h->count[]
+        let offs = new Array(nCounts+1);        // offsets in symbol table for each length
+        let length = new Array(256);            // code lengths
+
+        let huffman = {
+            count: new Int16Array(nCounts+1),
+            symbol: new Int16Array(nSymbols),
+            left: 0                             // number of possible codes left
+        };
+
+        /* convert compact repeat counts into symbol bit length list */
+        symbol = 0;
+        for (let i = 0; i < rep.length; i++) {
+            len = rep[i];
+            huffman.left = (len >> 4) + 1;
+            len &= 15;
+            do {
+                length[symbol++] = len;
+            } while (--huffman.left);
+        }
+        let n = symbol;
+
+        /* count number of codes of each length */
+        for (len = 0; len <= nCounts; len++) {
+            huffman.count[len] = 0;
+        }
+        for (symbol = 0; symbol < n; symbol++) {
+            (huffman.count[length[symbol]])++;  // assumes lengths are within bounds
+        }
+        if (huffman.count[0] == n) {            // no codes!
+            return huffman;                     // complete, but decode() will fail
+        }
+
+        /* check for an over-subscribed or incomplete set of lengths */
+        huffman.left = 1;                       // one possible code of zero length
+        for (len = 1; len <= nCounts; len++) {
+            huffman.left <<= 1;                 // one more bit, double codes left
+            huffman.left -= huffman.count[len]; // deduct count from possible codes
+            if (huffman.left < 0) {
+                return huffman;                 // over-subscribed (left > 0 means incomplete)
+            }
+        }
+
+        /* generate offsets into symbol table for each length for sorting */
+        offs[1] = 0;
+        for (len = 1; len < nCounts; len++) {
+            offs[len + 1] = offs[len] + huffman.count[len];
+        }
+
+        /*
+        * put symbols in table sorted by length, by symbol order within each length
+        */
+        for (symbol = 0; symbol < n; symbol++) {
+            if (length[symbol] != 0) {
+                huffman.symbol[offs[length[symbol]]++] = symbol;
+            }
+        }
+
+        /* left is zero for complete set, positive for incomplete set */
+        return huffman;
     }
 }
