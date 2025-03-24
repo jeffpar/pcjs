a5e97d93db31a6bc87ccecc9c2efa1eb59c88c97
Jeff Parsons <Jeff@pcjs.org>
2025-03-24 14:41:13 -0700

    More split.js improvements

--- tools/misc/split.js
+++ tools/misc/split.js
@@ -17,8 +17,8 @@
  *
  *      diskimage.js --all="*.{ZIP,ARC}" --list=csv
  *
- * this splits that CSV file into two smaller sets of files (archives-#.csv and files-#.csv),
- * making it easier to import the data into "archives" and "files" tables in a MySQL "software"
+ * this splits that CSV file into two smaller sets of files (arcs-#.csv and files-#.csv),
+ * making it easier to import the data into "arcs" and "files" tables in a MySQL "software"
  * database.
  *
  * Every line of the input CSV (well, that we care about) should have the following fields:
@@ -34,17 +34,28 @@
  * Every time we encounter a new archive, we push a line with the following fields to the
  * archiveLines array:
  *
- *      archiveID,hash,modified,attr,size,name,messages,comment
+ *      arcID,hash,modified,attr,size,name,messages,comment
  *
  * We then increment fileID and push a line with the following fields to the fileLines array:
  *
- *      fileID,archiveID,hash,modified,attr,size,compressed,method,name,path,messages,comment
+ *      fileID,arcID,hash,modified,attr,size,compressed,method,name,path,messages,comment
  *
  * Those lines must then be followed by every line that does NOT begin with the above values
  * (ie, any comment lines that follow it).
  *
  * Once we have pushed 500000 lines to fileLines, we write them to files-#.csv and empty the array;
- * ditto for archiveLines, which is written to archives-#.csv.
+ * ditto for archiveLines, which is written to arcs-#.csv.
+ *
+ * NOTES
+ *
+ * After importing all the split CSVs into MySQL, I noticed that the arcID of the first entry for
+ * each archive in the "files" CSVs was off by 1, so I fixed those arcIDs after-the-fact:
+ *
+ *      UPDATE files SET arcID = arcID + 1 WHERE method = 'None' AND (name LIKE '%.ZIP' OR name LIKE '%.ARC');
+ *
+ * Once that was fixed, I was then able to add a foreign key constraint to the "files" table:
+ *
+ *      ALTER TABLE files ADD CONSTRAINT fk_arc_id FOREIGN KEY (arcID) REFERENCES arcs(arcID) ON DELETE CASCADE ON UPDATE CASCADE;
  */
 import fs from "fs";
 import path from "path";
@@ -88,7 +99,7 @@ let archiveLines = [];
 let fileLines = [];
 let lastFileIndex = 0, lastFilePath = "";
 let archiveMode = false;
-let archiveID = 10000000, fileID = 20000000;
+let arcID = 10000000, fileID = 20000000;
 let longestFileName = "", longestPathName = "", longestMethod = "", longestMessage = "";
 
 let flushArchives = function() {
@@ -97,8 +108,8 @@ let flushArchives = function() {
         write = !nArchives;
         if (nArchives) nArchives--;
     }
-    let archiveCSV = `archives-${++nArchives}.csv`;
-    let text = (headers? "archiveID,hash,modified,attr,size,name,messages,comment\n" : "") + archiveLines.join("\n") + "\n";
+    let archiveCSV = `arcs-${++nArchives}.csv`;
+    let text = (headers? "arcID,hash,modified,attr,size,name,messages,comment\n" : "") + archiveLines.join("\n") + "\n";
     if (write) {
         console.log(`writing ${archiveCSV}...`);
         fs.writeFileSync(archiveCSV, text);
@@ -115,7 +126,7 @@ let flushFiles = function() {
         if (nFiles) nFiles--;
     }
     let fileCSV = `files-${++nFiles}.csv`;
-    let text = (headers? "fileID,archiveID,hash,modified,attr,size,compressed,method,name,path,messages,comment\n" : "") + fileLines.join("\n") + "\n";
+    let text = (headers? "fileID,arcID,hash,modified,attr,size,compressed,method,name,path,messages,comment\n" : "") + fileLines.join("\n") + "\n";
     if (write) {
         console.log(`writing ${fileCSV}...`);
         fs.writeFileSync(fileCSV, text);
@@ -136,6 +147,7 @@ for (let line of lines) {
         // The final capture group contains the remainder (ie, name, path, messages, and comment).
         //
         let hash = match[1];
+        if (hash == "--------------------------------") hash = "NULL";
         let modified = match[2];
         let attr = match[3];
         let size = match[4];
@@ -191,7 +203,7 @@ for (let line of lines) {
             }
             let pathNameQuoted = pathName.replace(/"/g, '""');
             if (pathNameQuoted.indexOf(',') >= 0 || pathNameQuoted == "NULL") pathNameQuoted = `"${pathNameQuoted}"`;
-            archiveLines.push(`${++archiveID},${prevMatch.hash},${prevMatch.modified},${prevMatch.attr},${prevMatch.size},${pathNameQuoted},${remainder}`);
+            archiveLines.push(`${++arcID},${prevMatch.hash},${prevMatch.modified},${prevMatch.attr},${prevMatch.size},${pathNameQuoted},${remainder}`);
             archiveMode = true;
         }
         else if (+size == -1) {
@@ -200,15 +212,8 @@ for (let line of lines) {
             //
         } else if (!hash) {
             //
-            // Lines without a hash indicate an unexpected error in the processing of an archive.  Here are some examples:
-            //
-            //      bdaa1858375b52a50d787c4d87a744ea,1989-07-23 04:24:16,32,17408,17408,None,FSP16.ZIP,/ibm0000-0009/ibm0001,,
-            //      ,,,,,,"FSP16.ZIP","/ibm0000-0009/ibm0001","bad archive",
-            //
-            //      ad69bf68553d39c403a540d10016af45,1990-05-10 23:21:12,32,201635,201635,Store,PC.ARC,/ibm0000-0009/ibm0007/POWERC-1.ZIP,,
-            //      ,,,,,,"PC.ARC","/ibm0000-0009/ibm0007/POWERC-1.ZIP","field type (0xa5) does not match any defined values",
-            //
-            // Our only goal here is to capture the error message, appending it to the file's previous entry.
+            // Lines without a hash indicate an unexpected error in the processing of an archive;
+            // our only goal here is to capture the error message, appending it to the file's previous entry.
             //
             // Messages, if any, are always quoted.  This differs from fileName and pathName fields, which are only quoted
             // if they contain commas.
@@ -261,7 +266,7 @@ for (let line of lines) {
             if (fileNameQuoted.indexOf(',') >= 0 || fileNameQuoted == "NULL") fileNameQuoted = `"${fileNameQuoted}"`;
             let pathNameQuoted = pathName.replace(/"/g, '""');
             if (pathNameQuoted.indexOf(',') >= 0 || pathNameQuoted == "NULL") pathNameQuoted = `"${pathNameQuoted}"`;
-            fileLines.push(`${++fileID},${archiveID},${hash},${modified},${attr},${size},${compressed},${method},${fileNameQuoted},${pathNameQuoted},${remainder}`);
+            fileLines.push(`${++fileID},${arcID},${hash},${modified},${attr},${size},${compressed},${method},${fileNameQuoted},${pathNameQuoted},${remainder}`);
             archiveMode = false;
         }
         prevMatch = {
@@ -289,7 +294,7 @@ if (fileLines.length) {
     flushFiles();
 }
 
-console.log(`longest file name: ${longestFileName} (${longestFileName.length})`);
-console.log(`longest path name: ${longestPathName} (${longestPathName.length})`);
-console.log(`longest method: ${longestMethod} (${longestMethod.length})`);
-console.log(`longest message: ${longestMessage} (${longestMessage.length})`);
+console.log(`longest file name: ${longestFileName.length} (${longestFileName})`);
+console.log(`longest path name: ${longestPathName.length} (${longestPathName})`);
+console.log(`longest method: ${longestMethod.length} (${longestMethod})`);
+console.log(`longest message: ${longestMessage.length} (${longestMessage})`);
